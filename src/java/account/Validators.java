13cdf3063c3e35292d73ab1a951825970bdced54
leon <ishimweleon@gmail.com>
2021-05-23 18:46:01 +0300

    new project

---
+++ src/java/account/Validators.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package account;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import org.w3c.dom.ls.LSResourceResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author leon
+ */
+@ManagedBean(name = "validators")
+public class Validators {
+    public void validateEmail(FacesContext context, UIComponent component, Object email){
+        System.out.println("here here"+email);
+        Boolean res = Pattern.matches(".*iuc.edu.tr", email.toString().trim());
+        if (!res)
+            throw new ValidatorException(new FacesMessage("email should belong to iuc.edu.tr"));
+    }
+    
+    public void validatePasswordConfirmation(FacesContext context, UIComponent component, Object confpassword) {
+        UserData data = (UserData)context.getExternalContext().getSessionMap().get("userData");
+        if(data == null) return;
+        if(data.getNewPassword().equals(confpassword)) return;
+        throw new ValidatorException(new FacesMessage("Passwords should match"));
+    }
+}
---
+++ src/java/config/LocalConfig.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import models.User;
+import org.apache.derby.client.am.DateTime;
+
+/**
+ *
+ * @author leon
+ */
+@ManagedBean(name = "localConfig")
+@SessionScoped
+public class LocalConfig {
+    private String minDate;
+    public User user;
+    private String confirmationCode;
+
+    public String getMinDate() {
+        if(minDate != null) return minDate;
+        LocalDate min = LocalDate.now().minusYears(16);
+        minDate = min.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        return minDate;
+    }
+    
+    public String getConfirmationCode(){
+        Random rnd = new Random();
+        confirmationCode = rnd.ints(6).toString();
+        return confirmationCode;
+    }
+    
+    public void validateConfirmationCode(FacesContext fc, UIComponent uic, String confCode){
+        
+    }
+    
+}
