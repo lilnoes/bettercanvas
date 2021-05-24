b855fea08efd8e3f71d53992034b3ff98930adc9
leon <ishimweleon@gmail.com>
2021-05-24 20:35:21 +0300

    added javafaker

--- src/java/utils/GeneralUtils.java
+++ src/java/utils/GeneralUtils.java
@@ -6,14 +6,30 @@
 package utils;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
 /**
  *
  * @author leon
  */
+@ManagedBean(name = "generalUtils")
 public class GeneralUtils {
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    private String token;
     public static void addMessage(FacesContext fct, String id, String message){
         fct.addMessage(id, new FacesMessage(message));
+    }
+    
+    public String loggedIn(){
+        System.out.println("here here here");
+        return "account/login.xhtml";
     }
 }
